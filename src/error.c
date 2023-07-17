75204c391ada08208f3fff0db1c4edcf0e2794b0
flauer <flauer@student.42heilbronn.de>
2023-07-17 10:01:11 +0200

    setting free'd pointers to NULL

--- src/error.c
+++ src/error.c
@@ -6,7 +6,7 @@
 /*   By: flauer <flauer@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 13:18:58 by flauer            #+#    #+#             */
-/*   Updated: 2023/07/12 15:17:56 by flauer           ###   ########.fr       */
+/*   Updated: 2023/07/17 09:57:07 by flauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,20 @@
 void	ft_err(t_instance *inst, const char *message)
 {
 	if (inst->mlx)
+	{
 		mlx_terminate(inst->mlx);
+		inst->mlx = NULL;
+	}
 	if (inst->map)
+	{
 		free_map(&inst->map);
+		inst->map = NULL;
+	}
 	if (inst->map_cpy)
+	{
 		free_map(&inst->map_cpy);
+		inst->map_cpy = NULL;
+	}
 	if (message)
 		ft_printf("%s\n", message);
 	exit(EXIT_FAILURE);
