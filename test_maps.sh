2504384b63d10e1d575d42aff20f374c5cfe2306
flauer <flauer@student.42heilbronn.de>
2023-07-20 12:28:29 +0200

    parsing test script

---
+++ test_maps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+./so_long maps/test/empty.ber
+./so_long maps/test/invalid_chars.ber
+./so_long maps/test/mult_exit.ber
+./so_long maps/test/mult_start.ber
+./so_long maps/test/no_all.ber
+./so_long maps/test/no_c.ber
+./so_long maps/test/no_p.ber
+./so_long maps/test/no_e.ber
+./so_long maps/test/no_route_c.ber
+./so_long maps/test/no_route_e.ber
+./so_long maps/test/not_ber.ner
+./so_long maps/test/not_closed.ber
+./so_long maps/test/not_rect.ber
+./so_long maps/test/small.ber
+./so_long maps/test/nonexistent.ber
